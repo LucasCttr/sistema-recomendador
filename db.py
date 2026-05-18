e05620d453baf0f8c75355ff5bdf92728870898f
LucasCrtt <carottalucas@gmail.com>
2026-05-18 18:24:39 -0300

    Update database URL to use recommender_with_tags.db for improved data management

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import relationship, sessionmaker, declarative_base
 import pandas as pd
 
 # Configuración de la base de datos SQLite
-DATABASE_URL = "sqlite:///./recommender.db"
+DATABASE_URL = "sqlite:///./recommender_with_tags.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
